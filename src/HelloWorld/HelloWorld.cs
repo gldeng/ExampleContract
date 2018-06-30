dc061c5973bf119b8e6361a694fb32bd7a9469b8
gldeng <realgldeng@gmail.com>
2018-06-30 18:28:45 +0800

    Add HelloWorld

---
+++ src/HelloWorld/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AElf.Sdk.CSharp;
+
+namespace HelloWorld
+{
+    public class HelloWorld : CSharpSmartContract
+    {
+        public string Greet()
+        {
+            return "Hello world!";
+        }
+    }
+}
